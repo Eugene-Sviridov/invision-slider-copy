ccb505867e5c31ae8698981ab49a0731cfc34783
Eugene-Sviridov <smart8410@gmail.com>
2018-03-03 18:56:45 +0300

    Add video

--- main.js
+++ main.js
@@ -2,13 +2,22 @@ document.addEventListener("DOMContentLoaded", function() {
     var playVideoBtns = document.querySelectorAll('.video-slider__item-btn'),
         peopleList = document.querySelector('.video-slider__people'),
         persons = document.querySelectorAll('.video-slider__person'),
-        sliders = document.querySelectorAll('.video-slider__item');
+        sliders = document.querySelectorAll('.video-slider__item'),
+        wrapper = document.querySelector('.video-slider'),
+        videoIframes = document.querySelectorAll('.video-slider__item-video'),
+        videoPlayers = [];
 
+    
+    Array.prototype.forEach.call(videoIframes, function(iframe) {
+        videoPlayers.push(new Vimeo.Player(iframe));
+    });
 
-    Array.prototype.forEach.call(playVideoBtns, function(btn) {
+    Array.prototype.forEach.call(playVideoBtns, function(btn, index) {
         btn.addEventListener('click', function() {
-            peopleList.classList.add('_video-open');
+            var slideIndex = Array.prototype.indexOf.call(playVideoBtns, btn);
 
+            videoPlayers[index].play();
+            wrapper.classList.add('_video-open');
             btn.parentElement.parentElement.classList.add('_content-hidden');
         });
     });
@@ -26,10 +35,14 @@ document.addEventListener("DOMContentLoaded", function() {
                     slider.classList.add('_active');
                 } else {
                     slider.classList.remove('_active');
+                    videoPlayers[index].pause();
+                    videoPlayers[index].setCurrentTime(0);
+                    
                 }
                 slider.classList.remove('_content-hidden');
             });
 
+            wrapper.classList.remove('_video-open');
             person.classList.add('_active');
         })
     });
